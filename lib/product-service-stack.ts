b9af75fd863fd4404cf27d90f0703dd64e4d3ef5
Lazy Goose <shamal.ma.rss@gmail.com>
2024-06-20 21:15:50 +0300

    refactor: aws-apigateway -> aws-apigatewayv2

--- lib/product-service-stack.ts
+++ lib/product-service-stack.ts
@@ -1,15 +1,16 @@
 import * as cdk from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as lambdaNode from "aws-cdk-lib/aws-lambda-nodejs";
-import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as apigateway from "aws-cdk-lib/aws-apigatewayv2";
+import * as apigatewayIntegrations from "aws-cdk-lib/aws-apigatewayv2-integrations";
 import { Construct } from "constructs";
 
 export class ProductServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const apiGateway = new apigateway.RestApi(this, "ProductServiceApi", {
-      restApiName: "Product Service",
+    const apiGateway = new apigateway.HttpApi(this, "ProductServiceApi", {
+      apiName: "Product Service",
     });
 
     const lambdaGetProductList = new lambdaNode.NodejsFunction(
@@ -32,18 +33,22 @@ export class ProductServiceStack extends cdk.Stack {
       }
     );
 
-    /* Resource: /products */
-    const productsResource = apiGateway.root.addResource("products");
-    productsResource.addMethod(
-      "GET",
-      new apigateway.LambdaIntegration(lambdaGetProductList)
-    );
+    apiGateway.addRoutes({
+      path: "/products",
+      methods: [apigateway.HttpMethod.GET],
+      integration: new apigatewayIntegrations.HttpLambdaIntegration(
+        lambdaGetProductList.node.id + "Integration",
+        lambdaGetProductList
+      ),
+    });
 
-    /* Resource: /products/{productId} */
-    const productByIdResource = productsResource.addResource("{productId}");
-    productByIdResource.addMethod(
-      "GET",
-      new apigateway.LambdaIntegration(lambdaGetProductById)
-    );
+    apiGateway.addRoutes({
+      path: "/products/{productId}",
+      methods: [apigateway.HttpMethod.GET],
+      integration: new apigatewayIntegrations.HttpLambdaIntegration(
+        lambdaGetProductById.node.id + "Integration",
+        lambdaGetProductById
+      ),
+    });
   }
 }
