4224893b872ffa831f7b7244cde9491129f630fb
Lazy Goose <shamal.ma.rss@gmail.com>
2024-06-23 11:44:32 +0300

    feat: create dynamodb tables

--- lib/product-service-stack.ts
+++ lib/product-service-stack.ts
@@ -3,11 +3,31 @@ import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as lambdaNode from "aws-cdk-lib/aws-lambda-nodejs";
 import * as apigateway from "aws-cdk-lib/aws-apigatewayv2";
 import * as apigatewayIntegrations from "aws-cdk-lib/aws-apigatewayv2-integrations";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import { Construct } from "constructs";
 
 export class ProductServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const tableBaseConfiguration = {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      pointInTimeRecovery: false,
+      billing: dynamodb.Billing.provisioned({
+        readCapacity: dynamodb.Capacity.fixed(5),
+        writeCapacity: dynamodb.Capacity.autoscaled({ maxCapacity: 15 }),
+      }),
+    } satisfies Omit<dynamodb.TablePropsV2, "partitionKey">;
+
+    const productTable = new dynamodb.TableV2(this, "ProductDynamodbTable", {
+      ...tableBaseConfiguration,
+      partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+    });
+
+    const stockTable = new dynamodb.TableV2(this, "StockDynamodbTable", {
+      ...tableBaseConfiguration,
+      partitionKey: { name: "product_id", type: dynamodb.AttributeType.STRING },
+    });
 
     const apiGateway = new apigateway.HttpApi(this, "ProductServiceApi", {
       apiName: "Product Service",
